b87a7a44ea4ea3c672d589d79f8dec2e5bafd213
bmenesca <bmenesca@grupolasegunda.com.ar>
2026-06-12 10:19:07 -0300

    3: crear entity Usuario

---
+++ backend/src/main/java/com/taller4/sistematurnos/usuario/Usuario.java
@@ -0,0 +1,31 @@
+package com.example.sistematurnos.entities;
+
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 100)
+	private String nombre;
+
+	@Column(nullable = false, unique = true, length = 100)
+	private String email;
+
+	@Column(name = "contrascena_hash", nullable = false, length = 255)
+	private String contraseñaHash;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole rol;
+
+	@Column(nullable = false)
+	private boolean activo = true;
+
+}
