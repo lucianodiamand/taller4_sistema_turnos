b6e51ceb210d9fd2024e027fb39423f4a37f0a5f
bmenesca <bmenesca@grupolasegunda.com.ar>
2026-06-12 10:18:24 -0300

    5: crear entity Servicio

---
+++ backend/src/main/java/com/taller4/sistematurnos/usuario/Servicio.java
@@ -0,0 +1,28 @@
+package com.taller4.sistematurnos.usuario;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Entity
+@Table(name = "servicios")
+@Data
+public class Servicio {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 100)
+	private String nombre;
+
+	@Column(columnDefinition = "TEXT")
+	private String descripcion;
+
+	@Column(name = "duracion_min", nullable = false)
+	private Integer duracionMin;
+
+	@Column(nullable = false, precision = 10, scale = 2)
+	private BigDecimal precio;
+
+}
